372bdf9571d4a6dd5a52a44c8637ecbe346211d8
antonio <antonio.nicasio.herrera@gmail.com>
2022-02-24 21:21:16 -0800

    bubble sort comentatios añandidos paso a paso

--- bubbleSort.js
+++ bubbleSort.js
@@ -8,15 +8,21 @@ const bubbleSort = function (array) {
 
   // recorremos el arreglo
   for (let i = 0; i < array.length; i++) {
+    // verificamos que el elemento sea mayor que el siguiente
     if (array[i] > array[i + 1]) {
+      // si es mayor, intercambiamos los elementos
       ;[array[i], array[i + 1]] = [array[i + 1], array[i]]
+      // y cambiamos el valor de swapped a true
       swapped = true
     }
   }
   // Usamos la recursividad
+  // Si no se hizo el cambio, terminamos la recursividad
   if (swapped) {
+    // si se hizo el cambio, volvemos a llamar a la funcion
     bubbleSort(array)
   }
+  // retornamos el arreglo
   return array
 }
 module.exports = bubbleSort
